7c649be516b7d49db6b899c695b617dcbcf56aff
DPooL2004 <dsrp002@gmail.com>
2023-11-10 11:12:54 -0500

    'inicio_de_proyecto'

---
+++ database/migrations/2023_11_10_153656_create_vuelos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVuelosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vuelos', function (Blueprint $table) {
+            $table->id();
+            $table -> integer('codigoVuelo');
+            $table -> string('lugarInicio');
+            $table -> string('destino');
+            $table -> integer('capacidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vuelos');
+    }
+}
